105ef46bfdb2e8d6f12c8019ffde2d749072a547
Younes <younesesebbar089@gmail.com>
2024-02-15 17:23:01 +0100

    resolve data used in autocomplete

--- src/app/book/autocomplete.resolver.ts
+++ src/app/book/autocomplete.resolver.ts
@@ -5,10 +5,22 @@ import {
 } from '@angular/router';
 import { SharedDataService } from './shared/sharedData.service';
 import { inject } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
 
 export const autocompleResolver: ResolveFn<any> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  return inject(SharedDataService).shareData();
+  const sharedDataService = inject(SharedDataService);
+  return sharedDataService.shareData().pipe(
+    map(({ authors, categories }) => {
+      sharedDataService.authorOptions = authors.map(
+        (author) => author.fullName
+      );
+      sharedDataService.categoryOptions = categories.map(
+        (category) => category.categoryName
+      );
+    }),
+    catchError((err) => throwError(() => console.log('Error Fetching data')))
+  );
 };
